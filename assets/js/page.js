0d1a4012c1ec00b9763ba316da1dbb966bb864fe
watchmedash <mojazer2017@gmail.com>
2024-11-26 08:51:19 +0400

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const currentLink = window.location.pathname; // Current post's link
 
-    fetch("/posts.json")
+    fetch("https://uaexpats.top/posts.json")
         .then(response => response.json())
         .then(posts => {
             // Find the current post index
